79ea781bc4ae0654b14bfa4f90cce1f36d42f062
Taylor Cool <taycool@gmail.com>
2019-10-29 14:33:16 -0700

    finished mvp

--- js/index.js
+++ js/index.js
@@ -12,13 +12,6 @@ busImg.addEventListener("mouseleave", () => {
     busImg.style.transition = "all 0.3s"
 });
 
-// click event
-document.querySelectorAll(".content-section").forEach(el => {
-    el.addEventListener("click", () => {
-        el.style.backgroundColor = "#add1dc"
-    })
-});
-
 // doubleclick event
 const boatImg = document.querySelector('.content-destination > img');
 boatImg.addEventListener("dblclick", () => {
@@ -32,7 +25,44 @@ window.addEventListener("resize", () => {
 })
 
 // scroll event
-const header = document.querySelector('.text-content > h2');   window.addEventListener("scroll", () => {
+const header = document.querySelector('.text-content h2');window.addEventListener("scroll", () => {
     header.style.color = "cornflowerblue";
     header.style.backgroundColor = "whitesmoke"
+});
+
+// keydown event
+const body = document.querySelector("body");
+body.addEventListener('keydown', () => {
+    body.style.display = 'none'
+});
+
+// keyup event
+body.addEventListener('keyup', () => {
+    body.style.display = 'inline'
+});
+
+// click event
+document.querySelectorAll(".content-section").forEach(el => {
+    el.addEventListener("click", () => {
+        el.style.backgroundColor = "#add1dc"
+    })
+});
+
+// click event propagation
+body.addEventListener('click', () => {
+  body.style.backgroundColor = "papayawhip";
+  console.log("end");
+});
+
+const content = document.querySelector(".content-section");
+content.addEventListener('click', () => {
+  content.style.backgroundColor = "whitesmoke";
+  console.log("middle")
+});
+
+const image = document.querySelector(".img-content");
+image.addEventListener('click', () => {
+  image.style.backgroundColor = "#FFAA42";
+  console.log("start");
+  stopPropogration();
 });
